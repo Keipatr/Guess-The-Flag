b6101d93f196d00ca0736f39a031b4af14cd8140
Kei <kpatrick@student.ciputra.ac.id>
2023-10-15 22:45:13 +0700

    initial

--- Guess The Game/ContentView.swift
+++ Guess The Game/ContentView.swift
@@ -8,14 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    var asean = ["Indonesia", "Singapore", "Malaysia", "Laos", "Philipines", "Cambodia", "Myanmar", "Thailand", "Brunei", "Vietnam"]
+    var angkaRandom = Int.random(in: 0...9)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color.mint
+                .ignoresSafeArea()
+            VStack{
+                Text("Pilih Bendera dari Negara : ")
+                    .foregroundStyle(.black)
+                Text(asean[angkaRandom])
+                    .foregroundStyle(.black)
+            }
+          
         }
-        .padding()
+        
+        HStack{
+            Spacer()
+            VStack{
+                ForEach(0..<5) { number in
+                    Button {
+                        
+                    } label: {
+                        
+                        Image(asean[number])
+                            .resizable()
+                            .frame(width:105,height: 65)
+                    }
+                }
+            }
+            Spacer()
+            VStack{
+                ForEach(5..<10) { number in
+                    Button {
+                        
+                    } label: {
+                        
+                        Image(asean[number])
+                            .resizable()
+                            .frame(width:105,height: 65)
+                    }
+                }
+            }
+            Spacer()
+        }
+        
+        
+    }
+    
+    func printConsole() {
+        print("Hai")
+    }
+    
+    func buttonBerbahaya(){
+        print("wow")
     }
 }
 
