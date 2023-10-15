2b3a8795eb531a507d8a2ffd8816f946a45c0436
Kei <kpatrick@student.ciputra.ac.id>
2023-10-15 22:19:42 +0700

    Initial Commit

---
+++ Guess The Game/Guess_The_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_GameApp.swift
+//  Guess The Game
+//
+//  Created by MacBook Pro on 15/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
